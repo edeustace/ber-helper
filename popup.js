004500952cfc12cc38df8bf9260ab6eb28e4a3af
Ed Eustace <ed.eustace@gmail.com>
2015-05-03 15:40:47 +0100

    first commit

---
+++ popup.js
@@ -0,0 +1,28 @@
+(function(){
+
+  function PropUtil(doc){
+
+    function getBerNo(){
+     var xpath = '//*[@id="smi-ber-details"]/../text()[2]';
+     var raw = document.evaluate(xpath, document.body, null, XPathResult.STRING_TYPE, null);
+     var match = raw.stringValue.match('BER No\\. ([0-9]*)');
+     if(match.length == 2){
+      return match[1]; 
+     } else {
+      return null;
+     }
+    }
+
+    var berNo = getBerNo();
+
+    
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    console.log('llll');
+
+    new PropUtil(document);
+  });
+
+
+})(this);
